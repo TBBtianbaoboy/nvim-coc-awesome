4a5fed28d2475c52132b1f16569f1753486d9ffb
aico <yyleng@vastaitech.com>
2022-08-17 13:26:22 +0800

    fix(copilot): make copilot auto startup

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -11,6 +11,16 @@ completion["neoclide/coc.nvim"] = {
             false)
     end
 }
-completion["github/copilot.vim"] = {opt = true, cmd = "Copilot"}
+completion["github/copilot.vim"] = {
+    opt = false,
+    -- opt == false, the cmd should be empty
+    -- cmd = "Copilot",
+    config = function()
+        -- lua运行vim配置文件
+        vim.api.nvim_exec(
+            [[source ~/.config/nvim/lua/modules/completion/copilot.vim]],
+            false)
+    end
+}
 
 return completion
