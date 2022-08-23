6e4651d2d9a805e649340507ba88e2b9968002cf
lengyangyu <admin@example.com>
2022-08-23 11:51:45 +0800

    feat(image): add image plugin config

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -156,4 +156,18 @@ function config.debugprint()
     require("debugprint").setup()
 end
 
+function config.image()
+    -- Require and call setup function somewhere in your init.lua
+    require('image').setup {
+        render = {
+            min_padding = 5,
+            show_label = true,
+            use_dither = true,
+        },
+        events = {
+            update_on_nvim_resize = true,
+        },
+    }
+end
+
 return config
